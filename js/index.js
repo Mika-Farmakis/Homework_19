b69f9e7d0569f92e2d9dfc561ce2a5348e8894da
Mika-Farmakis <mikafarmakis@gmail.com>
2020-06-27 22:39:51 -0700

    update

--- js/index.js
+++ js/index.js
@@ -3,13 +3,37 @@ $("myID").click(function(){
   });
 
 
-
   screen("myID").on("click",function(){
       console.log("Got to #myID");
   });
 
 
-  $(#myID).on("click",function(){
+  $(myID).on("click",function(){
       $(".fadeMe").fadeToggle("slow");
   })
 
+  function fade(){
+    $('.preloader').fadeOut("slow");
+}
+setTimeout(fade, 3000);
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
